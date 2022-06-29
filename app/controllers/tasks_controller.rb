b8eac73eea85c1fe9ab3f0885bfaea79e1e62a78
Jenipha Rachel <jenipharachel@gmail.com>
2022-06-29 16:36:54 +0530

    changed task to instance variables for show action to work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :load_task!, only: [:show, :update]
+  before_action :load_task!, only: %i[show update destroy]
 
   def index
     tasks = Task.all
@@ -9,27 +9,32 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
+    @task = Task.new(task_params)
+    @task.save!
     respond_with_success(t("successfully_created"))
   end
 
   def show
-    respond_with_json({ task: task })
+    respond_with_json({ task: @task })
   end
 
   def update
-    Task.update!(task_params)
+    @task.update!(task_params)
     respond_with_success(t("successfully_updated"))
+  end
+
+  def destroy
+    @task.destroy!
+    respond_with_json
   end
 
   private
 
     def load_task!
-      task = Task.find_by!(slug: params[:slug])
+      @task = Task.find_by!(slug: params[:slug])
     end
 
     def task_params
-      params.require(:slug).permit(:title)
+      params.require(:task).permit(:title)
     end
 end
